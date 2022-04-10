a6ff660029f799c53cb32d06b5352698e8060207
IsmatKabir <Auni858@gmail.com>
2022-04-10 16:52:11 -0400

    first commit

---
+++ src/demoB/DemoB2.java
@@ -0,0 +1,14 @@
+package demoB;
+
+public class DemoB2 {
+
+	public static void main(String[] args) {
+		System.out.println("this is practice java");
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/demoB/DemoB2B.java
@@ -0,0 +1,16 @@
+package demoB;
+
+public class DemoB2B {
+
+	public static void main(String[] args) {
+	System.out.println("this is practice");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
